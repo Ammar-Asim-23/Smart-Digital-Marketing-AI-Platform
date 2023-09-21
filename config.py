c5913a554f56f100ede6053259e302a860ac13cf
Ammar-Asim-23 <ammarasim065@gmail.com>
2023-09-21 11:54:16 +0500

    configuring repository

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+    # Flask
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///your-database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email
+    MAIL_SERVER = 'smtp.example.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'your-email@example.com'
+    MAIL_PASSWORD = 'your-email-password'
+    MAIL_DEFAULT_SENDER = 'your-email@example.com'
+
+    # Other settings
+    # ...
+
